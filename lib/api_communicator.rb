0bd7f7cc9f4cbf6493767de0f2f674a3f28d7a2c
Franco Cappitta <franco84@gmail.com>
2016-11-29 22:08:47 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,39 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  total_films = []
+  new_page = "http://www.swapi.co/api/people/"
+    while new_page != nil
+      all_characters = RestClient.get(new_page)
+      character_hash = JSON.parse(all_characters)
+        character_hash.each do |categories, characters|
+          if categories == "results"
+            characters.each do |profile|
+              if profile["name"].downcase == character
+                 total_films << profile["films"]
+               end
+             end
+           end
+         end
+         character_hash.each do |categories, characters|
+           if categories == "next"
+             new_page = characters
+           end
+         end
+    end
+    return total_films.flatten
 end
 
+
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |url, index|
+    all_films = RestClient.get(url)
+    film_hash = JSON.parse(all_films)
+    puts "#{index+1}. #{film_hash["title"]}"
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,17 @@
+require_relative 'api_communicator.rb'
+
 def welcome
-  # puts out a welcome message here!
+  "HI!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character:"
+  character = gets.chomp.downcase
+   until show_character_movies(character) != []
+    puts "Please enter another name:"
+    character = gets.chomp.downcase
+  end
 end
+
+welcome
+get_character_from_user
